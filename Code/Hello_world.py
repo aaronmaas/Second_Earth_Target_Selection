55177ed61740d29be120841ef7241e3b20867011
aaronmaas <aaron.wehlmann@gmail.com>
2023-04-27 22:12:27 +0200

    New Code

--- Code/Hello_world.py
+++ Code/Hello_world.py
@@ -5,9 +5,9 @@ import scipy as sp
 import emcee
 import parsec_py3_rp_dr3 as parsec
 
-source_id = 13821504093
 
-parsec.runit(source_id, isopath = "/Users/maas/Desktop/Second_Earth_Target_Selection/Data")
+
+parsec.runit("GJ3142")
 
 
 print("Works")
